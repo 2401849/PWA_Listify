cdadbdf1d4d958071222e4ca285c24be337ecbd7
2401849 <2401849@estudante.uab.pt>
2025-02-03 18:58:26 +0000

    adjust to app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {APP_GUARD} from "@nestjs/core";
 import {JwtAuthGuard} from "./auth/jwt-auth-guard";
@@ -15,6 +18,9 @@ import {JwtModule} from "@nestjs/jwt";
             }),
             inject: [ConfigService],
         }),
+        UsersModule,
+        TodosModule,
+        AuthModule,
         JwtModule.registerAsync({
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => ({
